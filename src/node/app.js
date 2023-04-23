47d7c640a598ad95774fa3e2a936a2f3f0e73b24
webfansplz <308241863@qq.com>
2023-04-23 20:28:13 +0800

    refactor(client): hook

--- src/node/app.js
+++ src/node/app.js
@@ -1,60 +1,54 @@
-const hook = window.__VUE_DEVTOOLS_GLOBAL_HOOK__
+// Reuse __VUE_DEVTOOLS_ instance first
+const hook = window.__VUE_DEVTOOLS_GLOBAL_HOOK__ ??= {
+  events: new Map(),
+  on(event, fn) {
+    if (!this.events.has(event))
+      this.events.set(event, [])
 
-const iframe = document.createElement('iframe')
-iframe.id = '_devtools'
-iframe.src = 'http://localhost:5173/__devtools/'
-iframe.style.position = 'fixed'
-iframe.style.bottom = '0'
-iframe.style.left = '50%'
-iframe.style.transform = 'translateX(-50%)'
-iframe.style.width = '1200px'
-iframe.style.height = '600px'
-// document.body.appendChild(iframe)
+    this.events.get(event).push(fn)
+  },
+  emit(event, ...payload) {
+    if (this.events.has(event))
+      this.events.get(event).forEach(fn => fn(...payload))
+  },
+}
 
+// print for iframe console
 window.print = (...s) => {
   console.log('print', ...s)
 }
-if (hook) {
-  // Reuse __VUE_DEVTOOLS_ instance
-  hook.on('app:init', async (app) => {
-    // console.log('Vue APP', app)
-    window.__VUE_DEVTOOLS_GET_VUE_INSTANCE__ = function () {
-      return app._instance
-    }
-    // const state = getInstanceState(app._instance)
-    // const walker = new ComponentWalker(500, null, true)
-    // const tree = await walker.getComponentTree(app._instance)
-    // console.log(app._instance)
-    // console.log(tree)
-    // console.log(getInstanceState(tree[0]?.children?.[0]?.instance))
-  })
-  hook.on('perf:end', async (app, _, component) => {
-    window.__VUE_DEVTOOLS_GET_VUE_INSTANCE__ = function () {
-      return component
-    }
-    const w = document.getElementById('_devtools')?.contentWindow
-    w?.postMessage('update', 'http://localhost:5173/__devtools/')
-  })
+
+const iframeId = '__vue_devtools_iframe__'
+
+function injectDevtools() {
+  const iframe = document.createElement('iframe')
+  iframe.id = iframeId
+  iframe.src = 'http://localhost:5173/__devtools/'
+  iframe.style.position = 'fixed'
+  iframe.style.bottom = '0'
+  iframe.style.left = '50%'
+  iframe.style.transform = 'translateX(-50%)'
+  iframe.style.width = '1200px'
+  iframe.style.height = '600px'
+  document.body.appendChild(iframe)
 }
-else {
-  // TODO: event emitter
-  window.__VUE_DEVTOOLS_GLOBAL_HOOK__ = {
-    on: async (event, payload, _, component) => {
-      if (event === 'app:init') {
-        window.__VUE_DEVTOOLS_GET_VUE_INSTANCE__ = function () {
-          return payload._instance
-        }
-      }
-      else if (event === 'perf:end') {
-        window.__VUE_DEVTOOLS_GET_VUE_INSTANCE__ = function () {
-          return component
-        }
-        const w = document.getElementById('_devtools')?.contentWindow
-        w?.postMessage('update', 'http://localhost:5173/__devtools/')
-      }
-    },
-    emit(event, ...payload) {
-      this.on(event, ...payload)
-    },
+
+hook.on('app:init', async (app) => {
+  window.__VUE_DEVTOOLS_GET_VUE_INSTANCE__ = function () {
+    return app._instance
   }
-}
+})
+
+hook.on('perf:start', async (_, __, component) => {
+  console.log('perf:start')
+})
+
+hook.on('perf:end', async (_, __, component) => {
+  window.__VUE_DEVTOOLS_GET_VUE_INSTANCE__ = function () {
+    return component
+  }
+  const contentWindow = document.getElementById(iframeId)?.contentWindow
+  contentWindow?.postMessage('update', 'http://localhost:5173/__devtools/')
+})
+
+injectDevtools()
