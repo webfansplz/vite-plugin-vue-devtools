d496ccd56a601426a131d8a63c547ce02a0c63c3
webfansplz <308241863@qq.com>
2023-04-24 23:28:03 +0800

    feat(client): ui-kit

---
+++ src/client/ui-kit/VIconTitle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  icon?: string
+  text?: string
+}>()
+</script>
+
+<template>
+  <div flex="~ gap-3" items-center>
+    <div v-if="icon" :class="icon" />
+    <slot>
+      <div>{{ text }}</div>
+    </slot>
+  </div>
+</template>
