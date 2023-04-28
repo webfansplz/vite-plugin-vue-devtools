75bd06724dc74842d5e129f2ebb0f1faf382c67c
webfansplz <308241863@qq.com>
2023-04-28 20:45:40 +0800

    feat: respect vue app devtools hide option

--- src/client/logic/hook.ts
+++ src/client/logic/hook.ts
@@ -10,14 +10,21 @@ const enum DevtoolsHooks {
   COMPONENT_EMIT = 'component:emit',
 }
 
+function hideInDevtools(app) {
+  return app?._vueDevtools_hidden_ || app._instance?.type?.devtools?.hide
+}
+
 export function connect() {
   hook.on(DevtoolsHooks.COMPONENT_INIT, (app) => {
+    // console.log('init', app)
+    if (app?._vueDevtools_hidden_)
+      return
     vueApp.value = app
     instance.value = app._instance
   })
 
   hook.on(DevtoolsHooks.COMPONENT_UPDATED, (app, uid, parentUid, component) => {
-    if (!app || (typeof uid !== 'number' && !uid) || !component)
+    if (!app || (typeof uid !== 'number' && !uid) || !component || hideInDevtools(app))
       return
 
     if (app) {
@@ -33,9 +40,10 @@ export function connect() {
   })
 
   hook.on(DevtoolsHooks.COMPONENT_ADDED, (app, uid, parentUid, component) => {
-    if (!app || (typeof uid !== 'number' && !uid) || !component)
+    if (!app || (typeof uid !== 'number' && !uid) || !component || hideInDevtools(app))
       return
 
+    console.log('added')
     if (app) {
       vueApp.value = app
       triggerRef(vueApp)
@@ -49,8 +57,10 @@ export function connect() {
   })
 
   hook.on(DevtoolsHooks.COMPONENT_REMOVED, (app, uid, parentUid, component) => {
-    if (!app || (typeof uid !== 'number' && !uid) || !component)
+    if (!app || (typeof uid !== 'number' && !uid) || !component || hideInDevtools(app))
       return
+
+    console.log('removed')
 
     if (app) {
       vueApp.value = app
@@ -65,8 +75,10 @@ export function connect() {
   })
 
   hook.on(DevtoolsHooks.COMPONENT_EMIT, (app, uid, parentUid, component) => {
-    if (!app || (typeof uid !== 'number' && !uid) || !component)
+    if (!app || (typeof uid !== 'number' && !uid) || !component || hideInDevtools(app))
       return
+
+    console.log('component:emit')
 
     if (app) {
       vueApp.value = app
