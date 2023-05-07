ee36e7837297a3cc09a0405a5d1ef04e5ad41bf9
webfansplz <308241863@qq.com>
2023-05-07 12:18:00 +0800

    feat: add virtual scroller for timeline event

--- src/client/components/TimelineEvent.vue
+++ src/client/components/TimelineEvent.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import dayjs from 'dayjs'
+import { RecycleScroller } from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
 defineProps<{
   data: any[]
@@ -11,7 +13,28 @@ const emits = defineEmits(['update-selected'])
 
 <template>
   <ul v-if="data.length">
-    <li
+    <RecycleScroller
+      v-slot="{ item, index }"
+      :items="data"
+      :min-item-size="40"
+      key-field="sortId"
+    >
+      <li
+        border="b base" h-10 flex cursor-pointer select-none items-center pl-3 pr-2 text-xs space-x-2
+        :class="[index === selected ? 'vue-block-active' : 'vue-block-hover']"
+        @click="emits('update-selected', index)"
+      >
+        <span flex-1 truncate font-mono space-x-1>
+          <span
+            font-medium
+            :class="[index === selected ? 'text-white' : 'text-purple-600 dark:text-purple-400']"
+          >{{ item.event.title }}</span>
+          <span opacity-75>{{ item.event.subtitle }}</span>
+        </span>
+        <span flex-none font-mono opacity-50>{{ dayjs(item.event.now).format('HH:mm:ss') }}</span>
+      </li>
+    </RecycleScroller>
+    <!-- <li
       v-for="(item, index) in data" :key="index"
       border="b base" h-10 flex cursor-pointer select-none items-center pl-3 pr-2 text-xs space-x-2
       :class="[index === selected ? 'vue-block-active' : 'vue-block-hover']"
@@ -25,7 +48,7 @@ const emits = defineEmits(['update-selected'])
         <span opacity-75>{{ item.event.subtitle }}</span>
       </span>
       <span flex-none font-mono opacity-50>{{ dayjs(item.event.now).format('HH:mm:ss') }}</span>
-    </li>
+    </li> -->
   </ul>
   <VPanelGrids v-else px5>
     <VCard flex="~ col gap2" min-w-30 items-center p3>
