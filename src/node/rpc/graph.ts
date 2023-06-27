853a1f40af3bd3c761c1e11385fd86889a9b6ed6
c0dedance <38075730+c0dedance@users.noreply.github.com>
2023-06-27 21:54:32 +0800

    chore: remove unused code (#79)

--- src/node/rpc/graph.ts
+++ src/node/rpc/graph.ts
@@ -5,28 +5,4 @@ export async function getComponentsRelationships(rpc: ViteInspectAPI['rpc']) {
   const modules = list?.modules || []
 
   return modules
-  const vueModules = modules.filter(i => i.id.match(/\.vue($|\?v=)/))
-
-  const graph = vueModules.map((i) => {
-    function searchForVueDeps(id: string, seen = new Set<string>()): string[] {
-      if (seen.has(id))
-        return []
-      seen.add(id)
-      const module = modules.find(m => m.id === id)
-      if (!module)
-        return []
-      return module.deps.flatMap((i) => {
-        if (vueModules.find(m => m.id === i))
-          return [i]
-        return searchForVueDeps(i, seen)
-      })
-    }
-
-    return {
-      id: i.id,
-      deps: searchForVueDeps(i.id),
-    }
-  })
-
-  return graph
 }
