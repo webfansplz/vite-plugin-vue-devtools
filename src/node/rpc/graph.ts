260156e533823044f7dfbda684d4b0c8e0d35e1a
webfansplz <308241863@qq.com>
2023-06-27 22:04:29 +0800

    chore: fix types

--- src/node/rpc/graph.ts
+++ src/node/rpc/graph.ts
@@ -1,6 +1,7 @@
 import type { ViteInspectAPI } from 'vite-plugin-inspect'
+import type { ModuleInfo } from '../../types'
 
-export async function getComponentsRelationships(rpc: ViteInspectAPI['rpc']) {
+export async function getComponentsRelationships(rpc: ViteInspectAPI['rpc']): Promise<ModuleInfo[]> {
   const list = await rpc.list()
   const modules = list?.modules || []
 
