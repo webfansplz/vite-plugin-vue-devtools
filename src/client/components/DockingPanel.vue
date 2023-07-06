34d514b6636ead7d1f03e5e5a5987753c6436e31
webfansplz <308241863@qq.com>
2023-07-06 09:01:01 +0800

    refactor: popup button (#114)

--- src/client/components/DockingPanel.vue
+++ src/client/components/DockingPanel.vue
@@ -4,16 +4,11 @@ import { isInPopup } from '../logic/state'
 
 <template>
   <div>
-    <div v-if="!isInPopup" px3 py2 border="b base" flex="~ col gap-1">
-      <div text-sm op50>
-        Dock devtools to
-      </div>
-      <PanelPosition />
-    </div>
     <div px3 py2 border="b base" flex="~ gap-2">
+      <PopupButton v-if="!isInPopup" />
       <VDarkToggle v-slot="{ toggle, isDark }">
         <VButton n="sm primary" @click="toggle">
-          <div carbon-sun translate-y--1px dark:carbon-moon />
+          <div carbon-sun dark:carbon-moon translate-y--1px />
           {{ isDark.value ? "Dark" : "Light" }}
         </VButton>
       </VDarkToggle>
