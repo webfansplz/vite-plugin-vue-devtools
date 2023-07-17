8750b8579d07e5f3b98dcdab4c8e9d7b46bcce23
webfansplz <308241863@qq.com>
2023-07-17 21:04:33 +0800

    workflow: copy-files script

---
+++ scripts/copy-files.ts
@@ -0,0 +1,13 @@
+import { copyFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// relative to scripts directory
+const destinations = [
+  ['../README.md', '../packages/node/README.md'],
+]
+
+const _filename = fileURLToPath(import.meta.url)
+destinations.forEach(([src, dest]) => {
+  copyFileSync(resolve(_filename, '..', src), resolve(_filename, '..', dest))
+})
