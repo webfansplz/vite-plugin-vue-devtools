ef96da9f16b53ec6a57a3eff8075db7e74b600be
webfansplz <308241863@qq.com>
2023-07-21 18:54:58 +0800

    fix: dynamic import estree-walker to avoid import failure

--- packages/core/src/compiler/common/ast.ts
+++ packages/core/src/compiler/common/ast.ts
@@ -1,14 +1,14 @@
 import type { BlockStatement, CallExpression, Node, ObjectMethod, ObjectProperty } from '@babel/types'
-import { walk } from 'estree-walker'
 import type { WalkerContext } from 'estree-walker/types/walker'
 import type { InsertLocation } from './parse'
 
 export type WalkCallback = (this: WalkerContext, node: Node, parent: Node | null, key: string | number | symbol | null | undefined, index: number | null | undefined) => void
 
-export function walkAST(node: Node, handlers: {
+export async function walkAST(node: Node, handlers: {
   enter?: WalkCallback
   leave?: WalkCallback
-}): Node | null {
+}): Promise<Node | null> {
+  const { walk } = await import('estree-walker')
   // @ts-expect-error estree-walker types are not compatible with babel types
   return walk(node, handlers)
 }
