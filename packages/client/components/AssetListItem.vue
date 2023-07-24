3155aa0a6a5464ff13a0a684939a596d500390bd
元吉/원길/OneGil <liyuanji1002@gmail.com>
2023-07-24 12:48:57 +0800

    feat(assets): tree view for list mode (#157)

--- packages/client/components/AssetListItem.vue
+++ packages/client/components/AssetListItem.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-const props = defineProps<{
-  asset: AssetInfo
-}>()
+const props = withDefaults(defineProps<{
+  item: any
+  index?: number
+  modelValue: AssetInfo | undefined
+}>(), {
+  index: 0,
+})
+
+const emit = defineEmits<{ (...args: any): void }>()
+const model = useVModel(props, 'modelValue', emit, { passive: true })
+
+const isCollection = computed(() => props.item?.children?.length)
+
+const open = ref(true)
 
 const icon = computed(() => {
+  if (isCollection.value)
+    return 'i-carbon-folder'
   const assets = {
     image: 'i-carbon-image',
     video: 'i-carbon-video',
@@ -12,15 +25,33 @@ const icon = computed(() => {
     text: 'i-carbon-document',
     json: 'i-carbon-json',
   }
-  return assets[props.asset.type] ?? 'i-carbon-document-blank'
+  return assets[props.item.type] ?? 'i-carbon-document-blank'
 })
 </script>
 
 <template>
-  <button flex="~ gap-1" w-full items-center hover="bg-active" rounded px4 py2>
-    <div :class="icon" />
-    <div of-hidden truncate ws-nowrap text-center>
-      {{ asset.path }}
-    </div>
-  </button>
+  <div>
+    <button
+      flex="~ gap-2" w-full items-center hover="bg-active" px4 py1
+      :style="{ paddingLeft: `calc(1rem + ${index * 1.5}em)` }"
+      :class="{ 'bg-active': !isCollection && model?.filePath === item?.filePath }"
+      @click="isCollection ? open = !open : model = item"
+    >
+      <div :class="icon" />
+      <span :class="{ 'flex items-center': isCollection }" flex-auto text-start text-sm font-mono>
+        {{ item.path }}
+      </span>
+      <VDIcon v-if="isCollection" icon="carbon:chevron-right" :transform-rotate="open ? 90 : 0" transition />
+    </button>
+    <div x-divider />
+    <slot v-if="open">
+      <AssetListItem
+        v-for="subItem in item?.children"
+        :key="subItem.filepath"
+        v-model="model"
+        :item="subItem"
+        :index="index + 1"
+      />
+    </slot>
+  </div>
 </template>
