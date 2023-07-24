fd2ce28ce171673fa90f59937e89ddd45c89b7a3
alexzhang1030 <1642114555@qq.com>
2023-07-24 14:21:23 +0800

    test: update snapshot

--- packages/core/src/compiler/__test__/index.test.ts
+++ packages/core/src/compiler/__test__/index.test.ts
@@ -8,10 +8,51 @@ test('trace-rerender', () => {
     ;import { getCurrentInstance as __VUE_DEVTOOLS_$getCurrentInstance__, onRenderTracked as __VUE_DEVTOOLS_$onRenderTracked__, onRenderTriggered as __VUE_DEVTOOLS_$onRenderTriggered__ } from 'vue'
     ;
 
+        ;const __VUE_DEVTOOLS_$colors__ = [
+        ['#ff000033', 100],
+        ['#7f000033', 50],
+        ['#ffff0033', 20],
+        ['#7f7f0033', 10],
+        ['#00800033', 5],
+        ['#00400033', 0],
+      ]
+
+      function __VUE_DEVTOOLS_$debounce__(fn, delay) {
+        let timer = null
+        return function () {
+          if (timer)
+            clearTimeout(timer)
+
+          timer = setTimeout(() => {
+            fn.apply(this, arguments)
+            timer = null
+          }, delay)
+        }
+      };
+
+      let __VUE_DEVTOOLS_$highlightEl__ = null
+      let __VUE_DEVTOOLS_$times__ = 0
+      const __VUE_DEVTOOLS_$debounceFn__ = __VUE_DEVTOOLS_$debounce__(() => {
+        document.body.removeChild(__VUE_DEVTOOLS_$highlightEl__)
+        __VUE_DEVTOOLS_$highlightEl__ = null
+        __VUE_DEVTOOLS_$times__ = 0
+      }, 3000)
+      
         
     ;__VUE_DEVTOOLS_$onRenderTriggered__((e) => {
           const instance = __VUE_DEVTOOLS_$getCurrentInstance__()
-          window.__VUE_DEVTOOLS_GLOBAL_HOOK__?.emit?.('render:triggered', e, instance)
+          window.__VUE_DEVTOOLS_GLOBAL_HOOK__?.emit?.('render:triggered', e, instance, (el) => {
+        if (__VUE_DEVTOOLS_$highlightEl__) {
+          __VUE_DEVTOOLS_$highlightEl__.style.backgroundColor = __VUE_DEVTOOLS_$colors__.find(([color, time]) => __VUE_DEVTOOLS_$times__ >= time)[0]
+          __VUE_DEVTOOLS_$times__ += 1
+        }
+        else {
+          el.style.backgroundColor = __VUE_DEVTOOLS_$colors__.at(-1)[0]
+          __VUE_DEVTOOLS_$highlightEl__ = el
+          document.body.appendChild(el)
+        }
+        __VUE_DEVTOOLS_$debounceFn__()
+      })
         });
 
         
