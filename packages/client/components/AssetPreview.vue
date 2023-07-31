6b21142176339b0b190f6c566ed2a8d9164f70cb
Jannchie <jannchie@gmail.com>
2023-07-31 21:46:28 +0900

    feat: video assets preview

--- packages/client/components/AssetPreview.vue
+++ packages/client/components/AssetPreview.vue
@@ -2,6 +2,7 @@
 defineProps<{
   asset: AssetInfo
   textContent?: string
+  detail?: boolean
 }>()
 </script>
 
@@ -18,6 +19,9 @@ defineProps<{
     <div v-else-if="asset.type === 'text' && !textContent" i-carbon-document text-3xl op20 />
     <div v-else-if="asset.type === 'text' && textContent" w-full self-start p4>
       <pre max-h-10rem of-hidden text-xs font-mono v-text="textContent" />
+    </div>
+    <div v-else-if="asset.type === 'video'">
+      <video :src="asset.publicPath" :autoplay="detail" :controls="detail" />
     </div>
     <div v-else i-carbon-help text-3xl op20 />
   </div>
