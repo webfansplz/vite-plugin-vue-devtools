6f2e7af0ca60a63cc46dcc014946d02b2191d5a0
alexzhang1030 <1642114555@qq.com>
2023-08-04 15:08:57 +0800

    feat: dockbar support drag

--- packages/client/components/GraphDock.vue
+++ packages/client/components/GraphDock.vue
@@ -6,7 +6,6 @@ const errorBoundaryMessages: Partial<Record<StateGraphStateEnum, string>> = {
   [StateGraphStateEnum.NOT_READY]: 'Is not ready yet.',
   [StateGraphStateEnum.NOT_SELECT_FILE]: 'You have not selected a file yet.',
   [StateGraphStateEnum.NOT_COLLECTED]: 'This file has not been collected or does\'t support.',
-  [StateGraphStateEnum.NO_STATE]: 'This file does not have any state that supported to analyze.',
 }
 
 const ErrorBoundary: FunctionalComponent<{
@@ -33,7 +32,7 @@ const activeTab = ref<(typeof tabs)[number]['name']>(tabs[0].name)
 </script>
 
 <template>
-  <ErrorBoundary v-if="stateGraphState !== StateGraphStateEnum.HAS_STATE" :state="stateGraphState" />
+  <ErrorBoundary v-if="errorBoundaryMessages[stateGraphState]" :state="stateGraphState" />
   <div v-else class="grid grid-rows-[30px_1fr] h-full w-full p2">
     <nav class="h30px w-full flex items-center gap1">
       <div v-for="item of tabs" :key="item.name" class="cursor-pointer p1" :class="[activeTab === item.name ? 'text-primary' : '']" @click="activeTab = item.name">
